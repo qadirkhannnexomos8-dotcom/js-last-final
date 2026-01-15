f9cd602579b4093bb1ee6efecea17520760518ff
qadirkhannnexomos8-dotcom <qadirkhannnexomos8@gmail.com>
2026-01-15 14:50:12 +0000

    done

---
+++ qadir/databases.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3 + 3) // we are using node js not browers
+
+console.log(3 
+    + 3); // code readalibity should be high
+
+
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => stand alone value
+    // undifined value
+    // symbol
---
+++ qadir/variables.js
@@ -0,0 +1,14 @@
+const accountId = 4049136
+let accountEmail ="qadir@gmail.com"
+var accountPassword = "859764"
+accountCity = "mirokhan"
+
+// accountId = 3 // not allowed
+accountEmail = "fahad@gamil.com"
+accountPassword = "03173014"
+accountCity = "canada"
+
+
+console.log(accountId);
+
+console.table( {accountId, accountEmail, accountPassword, accountCity})
